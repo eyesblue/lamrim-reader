44589c41f5eb176cd81a4d782332eaf2855c0627
kingofeyesblue <kingofeyesblue@gmail.com>
2012-05-02 04:58:50 +0000

    First release with About activity.

---  lamrim-reader --username kingofeyesblue@gmail.com/LamrimReader/src/eyes/blue/AboutActivity.java
+++  lamrim-reader --username kingofeyesblue@gmail.com/LamrimReader/src/eyes/blue/AboutActivity.java
@@ -3,11 +3,17 @@ package eyes.blue;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
-		TextView aboutTextView=(TextView) findViewById(R.id.aboutTextView);
-		aboutTextView.setText(Html.fromHtml(getString(R.string.aboutUs)));
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		TextView aboutTextTitle=(TextView) findViewById(R.id.aboutTextTitle);
+		aboutTextTitle.setText(R.string.app_name);
+		TextView aboutTextContent=(TextView) findViewById(R.id.aboutTextContent);
+		aboutTextContent.setMovementMethod(new ScrollingMovementMethod());
+		aboutTextContent.setText(Html.fromHtml(getString(R.string.aboutDesc)));
 	}
 }
